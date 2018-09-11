f982a1d2dd449446642098a2dabf07c854a6f91d
thegrondin <tomtom9461@gmail.com>
2018-09-10 23:17:29 -0400

    Initial Commit

---
+++ Coll_Mgmt_App/Coll_Mgmt_App/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Coll_Mgmt_App.Views;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace Coll_Mgmt_App
+{
+	public partial class App : Application
+	{
+		
+		public App ()
+		{
+			InitializeComponent();
+
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Coll_Mgmt_App/Coll_Mgmt_App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Coll_Mgmt_App.Models;
+
+namespace Coll_Mgmt_App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
