52e38631bea18cadd4de5333c39b7195ce83b1d6
Thomas Dion Grondin <tomtom999@hotmail.ca>
2018-09-15 02:48:27 -0400

    Starting making the CollectionViewModel and BaseViewModel created

--- Coll_Mgmt_App/Coll_Mgmt_App/ViewModels/CollectionViewModel.cs
+++ Coll_Mgmt_App/Coll_Mgmt_App/ViewModels/CollectionViewModel.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using Coll_Mgmt_App.Models;
+using Coll_Mgmt_App.Services.DataService;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Coll_Mgmt_App.ViewModels
 {
-    class CollectionViewModel
+
+    class CollectionViewModel : BaseViewModel
     {
+        private ICollectionRepository<CollectionModel> _collectionRepository;
+
+        public List<string> CollectionsName { get; set; }
+
+        public CollectionViewModel(ICollectionRepository<CollectionModel> collectionRepository)
+        {
+            _collectionRepository = collectionRepository;
+        }
+
+        async Task ExecuteLoadCollectionsNameCommand()
+        {
+            if (IsBusy) return;
+
+            IsBusy = true;
+
+            try
+            {
+                CollectionsName.Clear();
+
+                IEnumerable<object> collectionsName = await _collectionRepository.GetFieldsFromCollections(new List<string>() { "Name" });
+
+                foreach(var obj in collectionsName)
+                {
+                    PropertyInfo pi = obj.GetType().GetProperty("Name");
+                    CollectionsName.Add((string)pi.GetValue(obj, null));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
